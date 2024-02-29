92f5440d4d04f233c8d5a65a66ff4cf92b7e46bf
Tony Murray <subscriptions@ruadesign.org>
2024-02-29 13:42:32 +0100

    Update replace.sh
    Version 3 with copying to other folder and checking if image found or not

--- replace.sh
+++ replace.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-
+# Version 2 - copies files to another folder if they are not present in the target folder
 for filename in ./*.jpg; do
   base_name=$(basename ${filename})
 
-  name=$(find ../files/ -name "$base_name" )
-        echo "$filename"
-        echo "found in..."
-        echo "$name"
-        cp -i "$base_name" $name
+  name=$(find ../files -name "$base_name" )
+        if test -z "$name" 
+        then
+              echo "Image  $name not found"
+        else
+              echo "Replacing image $name"
+              cp -i "$base_name" $name
+        fi
 
+  if find  ../files -name "$base_name" | grep . ; then echo Found; else cp -i "$base_name" ../files/2024/02/; fi
 
 done
+
